f37314fe9331d6b532b1b407c0f9b8e60dffc488
Cam @ Camtoso <cameron_dwyer@hotmail.com>
2019-09-08 23:38:35 +1000

    installed packages and updated tsx

--- src/app/scripts/timelyMessageExtension/TimelyMessageExtensionAction.tsx
+++ src/app/scripts/timelyMessageExtension/TimelyMessageExtensionAction.tsx
@@ -12,12 +12,16 @@ import {
 } from "msteams-ui-components-react";
 import TeamsBaseComponent, { ITeamsBaseComponentProps, ITeamsBaseComponentState } from "msteams-react-base-component";
 import * as microsoftTeams from "@microsoft/teams-js";
+import * as moment from "moment-timezone";
+import Select from "react-select";
 
 /**
  * State for the TimelyMessageExtensionAction React component
  */
 export interface ITimelyMessageExtensionActionState extends ITeamsBaseComponentState {
-    email: string;
+    baseLocation: string;
+    baseTime: string;
+    timezonesConversions: Array<{locationName: string, time: string}>;
 }
 
 /**
@@ -31,13 +35,16 @@ export interface ITimelyMessageExtensionActionProps extends ITeamsBaseComponentP
  * Implementation of the Timely Message Extension Task Module page
  */
 export class TimelyMessageExtensionAction extends TeamsBaseComponent<ITimelyMessageExtensionActionProps, ITimelyMessageExtensionActionState> {
+    private selectedZones: Array<{locationName: string}> = new Array();
 
     public componentWillMount() {
+        this.initaliseLocations();
         this.updateTheme(this.getQueryVariable("theme"));
         this.setState({
-            fontSize: this.pageFontSize()
+            fontSize: this.pageFontSize(),
+            timezonesConversions: new Array(),
+            baseTime: this.currentDateTimeInLocalDateTimeFormat()
         });
-
         microsoftTeams.initialize();
         microsoftTeams.registerOnThemeChangeHandler(this.updateTheme);
     }
@@ -53,41 +60,77 @@ export class TimelyMessageExtensionAction extends TeamsBaseComponent<ITimelyMess
         const { rem, font } = context;
         const { sizes, weights } = font;
         const styles = {
-            header: { ...sizes.title, ...weights.semibold },
+            header: { ...sizes.title2, ...weights.semibold },
             section: { ...sizes.base, marginTop: rem(1.4), marginBottom: rem(1.4) },
-            footer: { ...sizes.xsmall }
+            footer: { ...sizes.xsmall },
+            selector: { minHeight: 180 },
+            list: {...sizes.base, marginLeft: 0, paddingLeft: 0}
         };
+
         return (
             <TeamsThemeContext.Provider value={context}>
                 <Surface>
                     <Panel>
                         <PanelHeader>
-                            <div style={styles.header}>Timely Message Extension configuration</div>
+                            <div style={styles.header}>Convert from</div>
                         </PanelHeader>
                         <PanelBody>
                             <div style={styles.section}>
-                                 <Input
+                                <Select
+                                    value={{value: this.state.baseLocation, label: this.state.baseLocation}}
+                                    options={this.getLocationOptions()}
+                                    onChange={(loc) => {
+                                        if (loc.label !== this.state.baseLocation) {
+                                            this.setState( {
+                                                baseLocation: loc.value
+                                            });
+                                        }
+                                        this.redrawSelectedTimezoneTable();
+                                    }}
+                                    isMulti={false}
+                                    maxMenuHeight={120}
+                                />
+                                <Input
+                                    type="datetime-local"
                                     autoFocus
-                                    placeholder="email@email.com"
-                                    label="Enter an e-mail address"
-                                    errorLabel={!this.state.email ? "This value is required" : undefined}
-                                    value={this.state.email}
+                                    label="Date / Time"
+                                    errorLabel={!this.state.baseTime ? "This value is required" : undefined}
+                                    value={this.state.baseTime}
                                     onChange={(e) => {
                                         this.setState({
-                                            email: e.target.value
+                                            baseTime: e.target.value
                                         });
+                                        this.redrawSelectedTimezoneTable();
                                     }}
                                     required />
                             </div>
-                            <div style={styles.section}>
-                                <PrimaryButton onClick={() => {
-                                    microsoftTeams.tasks.submitTask({
-                                        email: this.state.email
-                                    });
-                                }}>OK</PrimaryButton>
+                        </PanelBody>
+                        <PanelHeader>
+                            <div style={styles.header}>Convert to</div>
+                        </PanelHeader>
+                        <PanelBody>
+                            <div style={{...styles.section, ...styles.selector}} >
+                                <Select
+                                    options={this.getLocationOptions()}
+                                    onChange={this.newTargetLocationSelected.bind(this)}
+                                    isMulti={false}
+                                    maxMenuHeight={120}
+                                />
+                                <ul style={styles.list}>
+                                    {this.state.timezonesConversions.map((item, index) => (
+                                        <li>{item.locationName}   {item.time}</li>
+                                    ))}
+                                </ul>
                             </div>
                         </PanelBody>
                         <PanelFooter>
+                            <div style={styles.section}>
+                                <PrimaryButton onClick={() => {
+                                    microsoftTeams.tasks.submitTask({
+                                        timezonesConversions: this.state.timezonesConversions
+                                    });
+                                }}>OK</PrimaryButton>
+                            </div>
                             <div style={styles.footer}>
                                 (C) Copyright Camtoso
                             </div>
@@ -96,5 +139,51 @@ export class TimelyMessageExtensionAction extends TeamsBaseComponent<ITimelyMess
                 </Surface>
              </TeamsThemeContext.Provider>
         );
+    }
+
+    private getLocationOptions() {
+        const options: Array<{value: string, label: string}> = new Array();
+        const availableZones = moment.tz.names();
+        availableZones.forEach(option =>
+                options.push({value: option, label: option})
+            );
+        return options;
+    }
+
+    private initaliseLocations() {
+        const defaultZone = moment.tz.guess();
+        this.setState({baseLocation: defaultZone});
+    }
+
+    private newTargetLocationSelected(newLocation) {
+        this.selectedZones.push({locationName: newLocation.value});
+        this.redrawSelectedTimezoneTable();
+    }
+
+    private currentDateTimeInLocalDateTimeFormat() {
+        const now: Date = new Date();
+        const utcString: string = now.toISOString().substring(0, 19);
+        const year: number = now.getFullYear();
+        const month: number = now.getMonth() + 1;
+        const day: number = now.getDate();
+        const hour: number = now.getHours();
+        const minute: number = now.getMinutes();
+        const localDatetime: string = year + "-" +
+            (month < 10 ? "0" + month.toString() : month) + "-" +
+            (day < 10 ? "0" + day.toString() : day) + "T" +
+            (hour < 10 ? "0" + hour.toString() : hour) + ":" +
+            (minute < 10 ? "0" + minute.toString() : minute) +
+            utcString.substring(16, 19);
+        return localDatetime;
+    }
+
+     private redrawSelectedTimezoneTable() {
+        // rebuild timezone conversion array
+        const timezones: Array<{locationName: string, time: string}> = new Array();
+        timezones.push({locationName: this.state.baseLocation, time: moment.tz(this.state.baseTime, this.state.baseLocation).tz(this.state.baseLocation).format("llll")});
+        this.selectedZones.forEach( entry => {
+            timezones.push({locationName: entry.locationName, time: moment.tz(this.state.baseTime, this.state.baseLocation).tz(entry.locationName).format("llll")});
+        });
+        this.setState({timezonesConversions: timezones});
     }
 }
